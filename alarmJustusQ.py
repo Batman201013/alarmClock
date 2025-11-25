12dc8217d16fd990fe5b483e41f06ae83e841a01
Batman201013 <justus_quitter@mis-munich.de>
2025-11-25 21:25:13 +0100

    updated with string and snooze

--- alarmJustusQ.py
+++ alarmJustusQ.py
@@ -1,6 +1,6 @@
 """Simple Python script to set an alarm for a specific time.
-   When the alarm goes off, a random youtube video will be opened.
-   The possible youtube video URLs are taken from "youtube_alarm_videos.txt"
+   When the alarm goes off, a random string will be shown.
+   The possible Strings are taken from "alarm_names.txt"
 """
 
 import datetime
